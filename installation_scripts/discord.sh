b54a76477903f9eac2eeee71e25e82f081edbba5
Fernando karchiloff <fernando.kga@gmail.com>
2020-07-26 02:22:06 -0300

    adding nvm-node and ajusting discord julia

--- installation_scripts/discord.sh
+++ installation_scripts/discord.sh
@@ -3,6 +3,7 @@
 echo "Installing Discord..."
 echo `sudo apt update`
 echo `sudo apt install gdebi-core wget`
+echo `sudo mkdir ./installation_files/`
 echo `wget -O ./installation_files/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"`
 echo `sudo gdebi ./installation_files/discord.deb`
 echo `Done.`
