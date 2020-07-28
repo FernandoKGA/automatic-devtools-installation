a8f4b87df1b233759a38694eb4364221606c1cea
Fernando karchiloff <fernando.kga@gmail.com>
2020-07-28 00:07:21 -0300

    Install command 'yes' at discord script

--- installation_scripts/discord.sh
+++ installation_scripts/discord.sh
@@ -5,5 +5,5 @@ echo `sudo apt-get update`
 echo `sudo apt-get install wget -y`
 echo `sudo mkdir ./installation_files/`
 echo `wget -O ./installation_files/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"`
-echo `sudo apt-get install ./installation_files/discord.deb`
+echo `sudo apt-get install ./installation_files/discord.deb -y`
 echo 'Done.'
